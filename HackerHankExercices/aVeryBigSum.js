8367030e067c4447e85332cd6e9c38a7b5b1ef95
Leonardohas <leonardohas83@gmail.com>
2023-05-24 15:53:13 -0300

    Added the explanation of the challenge.

--- HackerHankExercices/aVeryBigSum.js
+++ HackerHankExercices/aVeryBigSum.js
@@ -1,3 +1,8 @@
+/*
+    Neste desafio, você precisa calcular e imprimir a soma dos elementos em um array,
+    levando em consideração que alguns desses inteiros podem ser bastante grandes.
+*/
+
 function aVeryBigSum(ar) {
     var sum = 0;
     var array = ar;
