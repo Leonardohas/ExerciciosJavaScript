7975c5754065f1a43b4426b20f5c9fbd5879c395
Leonardohas <leonardohas83@gmail.com>
2023-05-25 11:15:22 -0300

    Exercise solution

--- HackerHankExercices/staircase.js
+++ HackerHankExercices/staircase.js
@@ -1,7 +1,7 @@
 /*
     Detalhes da escada
     Esta é uma escada de tamanho n = 4
-    
+
        #
       ##
      ###
@@ -10,3 +10,14 @@
     Sua base e altura são iguais a n. Ela é desenhada usando símbolos de # e espaços. A última linha não é precedida por nenhum espaço.
     Escreva um programa que imprime uma escada de tamanho n. 
 */
+
+function staircase(n) {
+    const array = new Array(n).fill(' ');
+    while (array.indexOf(' ') != -1){
+        array.shift();
+        array.push('#')
+        console.log(array.join(''));
+        n--;
+    }
+    return array
+}
