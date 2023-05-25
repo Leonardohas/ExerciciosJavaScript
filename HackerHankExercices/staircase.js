5a574a186c059462aaef579d1a69a2b8ccfa4749
Leonardohas <leonardohas83@gmail.com>
2023-05-25 11:06:25 -0300

    Added the activity description

---
+++ HackerHankExercices/staircase.js
@@ -0,0 +1,12 @@
+/*
+    Detalhes da escada
+    Esta é uma escada de tamanho n = 4
+    
+       #
+      ##
+     ###
+    ####
+
+    Sua base e altura são iguais a n. Ela é desenhada usando símbolos de # e espaços. A última linha não é precedida por nenhum espaço.
+    Escreva um programa que imprime uma escada de tamanho n. 
+*/
