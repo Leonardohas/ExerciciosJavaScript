40a1be2e57b89c6c15a5d775009acd78a2aef5a7
Leonardohas <leonardohas83@gmail.com>
2023-05-25 10:42:10 -0300

    Exercise solution

--- HackerHankExercices/diagonalDifference.js
+++ HackerHankExercices/diagonalDifference.js
@@ -9,3 +9,13 @@ Por exemplo, a matriz quadrada "arr" é mostrada abaixo:
 A diagonal da esquerda para a direita = 1 + 5 + 9 = 15. A diagonal da direita para a esquerda = 3 + 5 + 9 = 17. 
 A diferença absoluta entre elas é 15 - 17 = 2.
 */
+
+function diagonalDifference(arr) {
+    const length = arr.length;
+    let diagonal1 = 0, diagonal2 = 0;
+    for(let i = 0; i < length; i++){
+         diagonal1 += arr[i][i];
+         diagonal2 += arr[length -1 - i][i]
+    }
+    return Math.abs(diagonal1 - diagonal2);  
+}
