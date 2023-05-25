ee378b30ad4c887009354121d93bfed6160d32d4
Leonardohas <leonardohas83@gmail.com>
2023-05-25 10:34:25 -0300

    Added the activity description

---
+++ HackerHankExercices/diagonalDifference.js
@@ -0,0 +1,11 @@
+/*
+Dada uma matriz quadrada, calcule a diferença absoluta entre as somas de suas diagonais. 
+Por exemplo, a matriz quadrada "arr" é mostrada abaixo:
+
+123
+456
+989
+
+A diagonal da esquerda para a direita = 1 + 5 + 9 = 15. A diagonal da direita para a esquerda = 3 + 5 + 9 = 17. 
+A diferença absoluta entre elas é 15 - 17 = 2.
+*/
