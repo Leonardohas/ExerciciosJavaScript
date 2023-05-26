23a5329e599a265cfcafd85f4891ecf72b3f5656
Leonardohas <leonardohas83@gmail.com>
2023-05-26 10:58:28 -0300

    added activity description

---
+++ HackerHankExercices/miniMaxSum.js
@@ -0,0 +1,9 @@
+/* 
+    Dado cinco números inteiros positivos, encontre os valores mínimo e máximo que podem ser calculados somando exatamente quatro dos cinco números inteiros. 
+    Em seguida, imprima os respectivos valores mínimo e máximo em uma única linha, separados por um espaço, como dois números inteiros longos.
+    
+    Exemplo:
+    arr = [1,3,5,7,9]
+    A soma minima é 1 + 3 +5 + 7 = 16 e a soma maxima é 3 + 5 + 7 + 9 = 24. A função printa 16 24.
+
+*/
