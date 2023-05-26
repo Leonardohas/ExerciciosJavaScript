e0f7dddb523d06688982170fbede1de4e22ce1be
Leonardohas <leonardohas83@gmail.com>
2023-05-26 11:02:34 -0300

    Exercise solution

--- HackerHankExercices/miniMaxSum.js
+++ HackerHankExercices/miniMaxSum.js
@@ -7,3 +7,24 @@
     A soma minima é 1 + 3 +5 + 7 = 16 e a soma maxima é 3 + 5 + 7 + 9 = 24. A função printa 16 24.
 
 */
+
+
+function miniMaxSum(arr) {
+    var sum = 0
+    var maxSum = 0
+    var minSum = 0
+    var minVal = arr[0]
+    var maxVal = arr[0]
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] > maxVal){
+            maxVal = arr[i]
+        }
+        if(arr[i] < minVal){
+            minVal = arr[i]
+        }
+        sum = sum + arr[i]     
+    }
+    maxSum = sum - minVal
+    minSum = sum - maxVal
+    console.log(`${minSum} ${maxSum}`)
+}
