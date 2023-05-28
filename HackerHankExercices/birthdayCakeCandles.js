29f289885d0e012ecb4221e2d9de950d4ae21f8a
Leonardohas <leonardohas83@gmail.com>
2023-05-28 18:44:23 -0300

    added activity description

---
+++ HackerHankExercices/birthdayCakeCandles.js
@@ -0,0 +1,10 @@
+/*
+    Você é responsável pelo bolo de aniversário de uma criança.
+    Você decidiu que o bolo terá uma vela para cada ano de idade total da criança.
+    A criança só conseguirá apagar a vela mais alta. Conte quantas velas são as mais altas.
+
+    Exemplo:
+
+    velas = [4,4,1,3]
+    As velas de altura máxima têm 4 unidades de altura. Existem 2 delas, portanto, retorne 2.
+*/
