558c52e998fe122c7e7499cbe3f7d5150c257885
Leonardohas <leonardohas83@gmail.com>
2023-05-29 11:20:12 -0300

    added activity description

---
+++ HackerHankExercices/timeConversion.js
@@ -0,0 +1,13 @@
+/*
+    Dado um horário no formato AM/PM de 12 horas, converta-o para o formato militar (24 horas).
+    Observação: - 12:00:00 AM em um relógio de 12 horas é 00:00:00 em um relógio de 24 horas.
+
+    Exemplo:
+
+    s = 12:01:00PM
+    Retorna 12:01:00
+
+    s = 12:01:00
+    Retorna 00:01:00
+*/
+
