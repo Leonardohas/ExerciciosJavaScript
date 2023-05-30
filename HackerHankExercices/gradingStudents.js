cace432dd1e3c2c1c3318650ec6f54d4627c0147
Leonardohas <leonardohas83@gmail.com>
2023-05-30 12:28:06 -0300

    added activity description

---
+++ HackerHankExercices/gradingStudents.js
@@ -0,0 +1,6 @@
+/*
+    cada estudante recebe uma nota de 1 até 100
+    se a nota for menor do que 40 (Reprovado)
+    se a diferença entre as notas e o proximo multiplo de 5 for menor que 3 então a nota será arredondada para cima, para o proximo multiplo de 5 
+    Se o valor da nota for menor que 38 não há arredondamento já que o resultado será reprovação.
+*/
