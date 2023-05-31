a618301f57954f347d2bf7afbbaaa054593921ca
Leonardohas <leonardohas83@gmail.com>
2023-05-31 11:47:22 -0300

    added activity description

---
+++ HackerHankExercices/applesAndOrange.js
@@ -0,0 +1,8 @@
+/*
+    A região vermelha signica a casa, onde (s) é o ponto de inicio e (t) o ponto final, a arvore de maça é a esquerda da casa e a de laranja é a direita
+    Assumindo que as arvores estão localizadas em um ponto unico, onde a arvore de maças é o ponto (a) e a de laranja o ponto (b)
+    Quando uma frutta cai de sua arvore, ela pousa em d unidades de distancia de sua arvore de origem ao longo do eixo x
+    Um valor negativo de d significa que a fruta caiu in d unidades da arvore a esquerda e e um valor positivo de d significa que caiu d unidades da arvore a direita
+    Dado o valor de d par m amaças e n oranges, determine quantas maças e laranjas irá cair na casa de sam.
+*/
+
