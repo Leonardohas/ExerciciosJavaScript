557ddfa73e8ad45cb84b424bfe74e68b21f56096
Leonardohas <leonardohas83@gmail.com>
2023-06-01 11:55:36 -0300

    added activity description

---
+++ HackerHankExercices/numberLineJumps.js
@@ -0,0 +1,17 @@
+/*
+
+Você é um coreografista de um show de circo com varios animais. Para um ato, lhe foi dado 2 cagurus em uma linha numerica pronta para pular em uma direção positiva.
+• O primeiro canguru começa na localização x1 e se move a uma taxa v1 metros por salto.
+• O segundo canguru começa na localização x2 e se move a uma taxa v2 metros por salto.
+
+Você tem que descobrir um jeito de pegar os dois cangurus na mesma localização no mesmo tempo como parte do show. se for possivel retorne YES, caso contrario retorne NO.
+
+Exemplo:
+x1 = 2
+v1 = 1
+x2 = 1
+v2 = 2 
+
+Depois de um pulo, eles estão ambos em x = 3, (x1 + v1 = 2 + 1, x2 + v2 = 1 + 2), então a resposta é YES
+
+*/
