21d45d01410ee07c81d42ffc63f9405b3fa73250
Leonardohas <leonardohas83@gmail.com>
2023-06-02 13:47:41 -0300

    added activity description

---
+++ HackerHankExercices/betweenTwoSets.js
@@ -0,0 +1,12 @@
+/* 
+    Havera dois arrays de inteiros. Determine todos os inteiros que satisfaz as seguintes condições.
+    1. Os elementos do primeiro array são todos fatores do inteiro sendo considerado
+    2. O inteiro sendo considerado é um fator de todos os elementos da segunda matriz
+    Esses numeros são referenciados por estar entre os dois arrays. Determine quantos desses números existem.
+    Exemplo:
+    a = [2,6]
+    b = [24,36]
+    Há dois numeros entre os arrays: 6 e 12
+    6 % 2 = 0,6 % 6 = 0,24 % 6 = 0 e 36 % 6 = 0 para o primeiro valor 
+    12 % 2 = 0,12 % 6 = 0 e 24 % 12 = 0,36 % 12 = 0 para o segundo valor, retorna 2  
+*/
