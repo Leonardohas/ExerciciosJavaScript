785d98f1a1774c118bcae63ef41b435da012ec35
Leonardohas <leonardohas83@gmail.com>
2023-06-06 11:45:25 -0300

    added activity description

---
+++ HackerHankExercices/breakingTheRecords.js
@@ -0,0 +1,18 @@
+/*
+
+Maria joga basquete no colegio, e quer ser pro. Cada temporada ela mantém um registro de seu jogo. Ela tabula o número de vezes que quebrou seu recorde da temporada para mais pontos e menos pontos em um jogo.
+pontos marcados no primeiro jogo estabelece seu registro para a temporada, e ela começa a contagem de lá
+
+Exemplo:
+
+pontos = [12,24,10,24]
+                                 Count
+jogo  pontos  minimo  maximo    min  max
+0       12      12      12       0    0     
+1       24      12      24       0    1
+2       10      10      24       1    1
+3       24      10      24       1    1
+
+Dado os registros das temporadas, determine o numero de vezes que maria quebra esse registro para maior e menor pontos marcados durante a temporada.
+
+*/ 
