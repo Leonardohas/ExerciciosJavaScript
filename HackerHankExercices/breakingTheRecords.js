dea353ad042445417c8c5f4f8eb95c95459dbf62
Leonardohas <leonardohas83@gmail.com>
2023-06-06 11:46:17 -0300

    activity solution

--- HackerHankExercices/breakingTheRecords.js
+++ HackerHankExercices/breakingTheRecords.js
@@ -16,3 +16,27 @@ jogo  pontos  minimo  maximo    min  max
 Dado os registros das temporadas, determine o numero de vezes que maria quebra esse registro para maior e menor pontos marcados durante a temporada.
 
 */ 
+
+function breakingRecords(scores) {
+    var firstGameMaxpoints = scores[0]; // 10
+    var firstGameMinPoints = scores[0]; // 10
+    var countMaxGames = 0;
+    var countMinGames = 0;
+ 
+    for (let index = 0; index < scores.length; index++) {
+         if (scores[index] > firstGameMaxpoints) {
+             firstGameMaxpoints = scores[index]
+             countMaxGames += 1
+         }
+         
+         if (scores[index] < firstGameMinPoints) {
+             firstGameMinPoints = scores[index]
+             countMinGames += 1
+         }
+    }
+ 
+    return [countMaxGames, countMinGames]
+ 
+ };
+ 
+ console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25,  1]))
